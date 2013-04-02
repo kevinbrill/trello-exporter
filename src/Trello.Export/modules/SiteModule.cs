213d9cff84c21f3d3d1b6584d4e9572ee49a3d9d
Kevin Brill <kevin.brill@gmail.com>
2013-04-02 12:43:39 -0400

    Tweaked APIs a bit.  Added select to default page

--- src/Trello.Export/modules/SiteModule.cs
+++ src/Trello.Export/modules/SiteModule.cs
@@ -9,6 +9,7 @@ namespace Trello.Export.Web.modules
     public class SiteModule : NancyModule
     {
         private readonly TrelloNet.Trello trello;
+        private const string Orginization = "researchteam2";
 
         public SiteModule()
         {
@@ -19,9 +20,12 @@ namespace Trello.Export.Web.modules
                 {
                     var boards = GetBoards();
 
+                    var primaryBoard = boards.FirstOrDefault(x => x.Id == "511cdb9c5984dad16a0021c9");
+
                     dynamic model = new
                         {
-                            Boards = boards
+                            Boards = boards,
+                            Lists = GetLists(primaryBoard.Id)
                         };
 
                     return View["views/default", model];
@@ -29,12 +33,7 @@ namespace Trello.Export.Web.modules
 
             Get["/api/boards"] = o => GetBoards();
 
-            Get["/api/boards/{id}/lists"] = o =>
-                {
-                    var board = trello.Boards.WithId(o.id);
-
-                    return trello.Lists.ForBoard(board);
-                };
+            Get["/api/boards/{id}/lists"] = o => GetLists(o.id);
 
             Get["/api/boards/{boardId}/lists/{listId}/cards"] = o =>
                 {
@@ -48,11 +47,18 @@ namespace Trello.Export.Web.modules
 
         private List<dynamic> GetBoards()
         {
-            var organization = trello.Organizations.WithId("researchteam2");
+            var organization = trello.Organizations.WithId(Orginization);
 
             var boards = trello.Boards.ForOrganization(organization);
 
-            return boards.Select(x => new {x.Id, x.Name}).ToList<dynamic>();
+            return boards.OrderBy(x => x.Name).Select(x => new {x.Id, x.Name}).ToList<dynamic>();
+        }
+        
+        private List<dynamic> GetLists(string boardId)
+        {
+            var board = trello.Boards.WithId(boardId);
+
+            return trello.Lists.ForBoard(board).ToList<dynamic>();
         }
     }
 }
